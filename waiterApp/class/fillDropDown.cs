edc97525a06ac35cc8ac60016350cdeaa87e2859
tuncayy <tuncayyilmaz5@gmail.com>
2018-04-11 18:16:41 +0300

    dropdown class ı eklendi

---
+++ waiterApp/class/fillDropDown.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+namespace waiterApp
+{
+
+    public class fillDropDown
+{
+        string connectionString = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+        public DataTable currency()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from currency", con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            return dt;
+        }
+        public DataTable languages()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from languages", con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            return dt;
+        }
+        public DataTable countries()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from countries", con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            return dt;
+        }
+        public DataTable states(int countryid)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from states where country_id = " + countryid, con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            return dt;
+        }
+
+
+    }
+}
