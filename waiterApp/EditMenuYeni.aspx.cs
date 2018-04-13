65a8906da2db2bded92f5a3ce5fc61af06334608
mestanli1996 <mertmestanli@gmail.com>
2018-04-14 00:09:42 +0300

    Edit kısmı tamamlandı. Jquery İşlemini yarın yapıcam.

---
+++ waiterApp/EditMenuYeni.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+namespace waiterApp
+{
+    public partial class EditMenuYeni : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+        fillDropDown fdp = new fillDropDown();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (!Page.IsPostBack)
+            {
+                Repeater1.DataSource = fdp.menucategories();
+                Repeater1.DataBind();
+
+            }
+        }
+
+        protected void Repeater1_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            
+            SqlCommand komut = new SqlCommand("SELECT * FROM [business].[menudetails] where catID="+Convert.ToInt32(DataBinder.Eval(e.Item.DataItem,"catID")), con);// işletmeye özel sorgu için businessID GİRİLECEK eKSİK
+            con.Open();
+            Repeater rp = (Repeater)e.Item.FindControl("Repeater2");
+            rp.DataSource = komut.ExecuteReader();
+            rp.DataBind();
+            komut.Dispose();
+            con.Close();
+        }
+
+
+
+
+    }
+}
